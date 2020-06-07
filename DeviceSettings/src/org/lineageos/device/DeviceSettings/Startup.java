dd9fa4fa0e6c1cfa089b59dfe629b8fa10685cf9
tumayouzi <tumayouzi@gmail.com>
2020-06-07 23:00:10 +0900

    Revert "DeviceSettings: Add support for automatic High brightness mode"
    This reverts commit 3e9898f1eaee37ecc4b274efda59fe813d622f25.

--- DeviceSettings/src/org/lineageos/device/DeviceSettings/Startup.java
+++ DeviceSettings/src/org/lineageos/device/DeviceSettings/Startup.java
@@ -77,8 +77,6 @@ public class Startup extends BroadcastReceiver {
         mHBM = false;
         restore(SingleTapSwitch.getFile(), enabled);
         }
-
-        Utils.enableService(context);
     }
 
     private void restore(String file, boolean enabled) {
